5c68c776220fc0000aa288890a3d2d0567a8dc74
MAwais-Ahmad <awais.ahmad.de@gmail.com>
2026-01-29 22:04:12 +0500

    feat: update app version to 2.0

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req,res) => res.send(`Hello from UMT DevSecOps Workshop! Host: ${process.env.HOSTNAME || 'local'}`));
+app.get('/', (req,res) => res.send(`Hello from UMT DevSecOps Workshop v2.0! Host: ${process.env.HOSTNAME || 'local'}`));
 app.get('/health', (req,res) => res.status(200).json({status:'ok'}));
 app.get('/error', (req,res) => { // useful to trigger alerts
   res.status(500).send('forced error for demo');
